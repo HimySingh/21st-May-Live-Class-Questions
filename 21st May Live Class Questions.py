08267e0cd36dee9e04ce43e808001fe5b3c31fcb
Himanshu Singh <96868684+HimySingh@users.noreply.github.com>
2022-05-27 14:19:08 +0530

    Add files via upload

---
+++ 21st May Live Class Questions.py
@@ -0,0 +1,26 @@
+#Questions
+
+list = [3,4,5,6,7 , [23,456,67,8,78,78] , [345,56,87,8,98,9] , (234,6657,6) , {"key1" :"sudh" , 234:[23,45,656]}]
+
+# 1 . Try to reverse a list.
+list1= [23,64,7,1,89,'Gabbar','Basanti', 3.6,64.34, [32,'Him',"Dance"]]
+print(list1[::-1])
+
+# 2.  Try to access 234 out of this list.
+print(list[7][0])
+
+# 3 . Try to access 456.
+print(list[5][1])
+
+# 4 . Try to extract only a list collection form list l.
+print(list[5])
+print(list[8][234])
+
+# 5 . Try to extract "sudh".
+print(list[8]["key1"])
+
+# 6 . Try to list all the key in dict element avaible in list.
+print((list[8]).keys())
+
+# 7 . Try to extract all the value element form dict available in list.
+print((list[8]).values())
